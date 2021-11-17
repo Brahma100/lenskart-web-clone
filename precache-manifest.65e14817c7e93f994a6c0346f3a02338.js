98611fb3ef8a4c55a9aa2e6718fef84f21f990fb
Brahma100 <brahma.jaiswal100@gmail.com>
2021-11-17 11:31:48 +0530

    Updates

---
+++ precache-manifest.65e14817c7e93f994a6c0346f3a02338.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb951bb3cde338cbf67e2883df2d0562",
+    "url": "/lenskart-web-clone/index.html"
+  },
+  {
+    "revision": "56cca8009d5b01e7c7c9",
+    "url": "/lenskart-web-clone/static/css/2.cdf836bb.chunk.css"
+  },
+  {
+    "revision": "84c41f321b2797442637",
+    "url": "/lenskart-web-clone/static/css/main.e560af50.chunk.css"
+  },
+  {
+    "revision": "56cca8009d5b01e7c7c9",
+    "url": "/lenskart-web-clone/static/js/2.e956f11f.chunk.js"
+  },
+  {
+    "revision": "174efde0f6112f58c0d6a62fc2e1fad1",
+    "url": "/lenskart-web-clone/static/js/2.e956f11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c41f321b2797442637",
+    "url": "/lenskart-web-clone/static/js/main.bab9b547.chunk.js"
+  },
+  {
+    "revision": "21cd7ff4582f798da78c",
+    "url": "/lenskart-web-clone/static/js/runtime-main.dfb85662.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/lenskart-web-clone/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/lenskart-web-clone/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/lenskart-web-clone/static/media/revicons.2feb69cc.eot"
+  }
+]);
